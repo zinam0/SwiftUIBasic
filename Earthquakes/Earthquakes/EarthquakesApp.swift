48bbf75b121d0d050f0a2294f0babde056820348
ZiZi <gamdev_93@naver.com>
2024-08-07 01:14:31 +0900

    push

---
+++ Earthquakes/Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Zizi on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Quakes()
+        }
+    }
+}
---
+++ Earthquakes/Earthquakes/Views/Quakes.swift
@@ -0,0 +1,18 @@
+//
+//  Quakes.swift
+//  Earthquakes
+//
+//  Created by Zizi on 8/6/24.
+//
+
+import SwiftUI
+
+struct Quakes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Quakes()
+}
