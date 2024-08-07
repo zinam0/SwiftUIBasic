4747913f141dee96cdc92f1faeef5f6283579390
ZiZi <gamdev_93@naver.com>
2024-08-07 23:58:41 +0900

    init

---
+++ FriendsFavoriteMovies/FriendsFavoriteMovies/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FriendsFavoriteMovies
+//
+//  Created by Zizi on 8/7/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
